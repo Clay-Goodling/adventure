5c0a51347e2a0a4a3375f749030b5cbc30a40533
Clay Goodling <clay@goodling.dev>
2020-03-17 20:16:31 -0400

    state module

---
+++ src/State.hs
@@ -0,0 +1,56 @@
+{- |Representation of dynamic adventure state.
+
+   This module represents the state of an adventure as it is being played,
+   including the adventurer's current room, the rooms that have been visited,
+   and functions that cause the state to change.
+-}
+module State
+  ( T
+  , init_state
+  , current_room_id
+  , get_visited
+  , go
+  ) where
+
+import qualified Adventure as Adv
+
+{- |The concrete type of values representing the game state. -}
+data State = State { currentRoom :: Adv.RoomId
+                   , visited :: [Adv.RoomId]
+                   } deriving Show
+
+{- |The abstract type of values representing the game state. -}
+newtype T = T State
+
+{- |[init_state a] is the initial state of the game when playing adventure [a].
+    In that state the adventurer is currently located in the starting room,
+    and they have visited only that room. -}
+init_state :: Adv.T -> T
+init_state adv = T $ State { currentRoom = room
+                           , visited = [room]
+                           } where
+                             room = Adv.start_room adv
+
+{- |[current_room_id st] is the identifier of the room in which the adventurer
+    currently is located in state [st]. -}
+current_room_id :: T -> Adv.RoomId
+current_room_id (T st) = currentRoom st
+
+{- |[visited st] is a set-like list of the room identifiers the adventurer has
+    visited in state [st]. The adventurer has visited a room [rm] if their
+    current room location is or has ever been [rm]. -}
+get_visited :: T -> [Adv.RoomId]
+get_visited (T st) = visited st
+{- |[go exit adv st] is [r] if attempting to go through exit [exit] in state
+    [st] and adventure [adv] results in [r].  If [exit] is an exit from the
+    adventurer's current room, then [r] is [Legal st'], where in [st'] the
+    adventurer is now located in the room to which [exit] leads.  Otherwise,
+    the result is [Illegal].
+    Effects: none.  [go] is not permitted to do any printing. -}
+go :: Adv.ExitName -> Adv.T -> T -> Maybe T
+go exit adv (T st) = do
+  room <- Adv.next_room adv (currentRoom st) exit
+  return $ T $ State { currentRoom = room
+                     , visited = if elem room v then v else room:v
+                     } where
+                       v = visited st
